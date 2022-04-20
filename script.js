76f66178077c4fa5f7bd8189f1a9e6dca3e41bdc
Luke Wilson <l2wilson94@gmail.com>
2022-04-20 19:02:43 +0100

    rotation now centered around centre of painter

--- script.js
+++ script.js
@@ -36,22 +36,41 @@ const makePainter = ({
 		minSpeed,
 		acceleration,
 		isPainting: false,
+		brushdx: 0,
+		brushdy: 0,
 		r: 0,
-		dr: 0,
 	}
 	return painter
 }
 
 const drawPainter = (context, painter) => {
 	const {image, scale, r} = painter
-	const x = painter.x + painter.offsetX * scale
-	const y = painter.y + painter.offsetY * scale
 	const [width, height] = ["width", "height"].map(dimension => image[dimension] * scale)
-	context.translate(x, y)
+
+	const center = {x: painter.x + width/2, y: painter.y + height/2}
+
+	context.translate(center.x, center.y)
 	context.rotate(r)
-	context.drawImage(image, 0, 0, width, height)
+	context.drawImage(image, -width/2, -height/2, width, height)
 	context.rotate(-r)
-	context.translate(-x, -y)
+	context.translate(-center.x, -center.y)
+}
+
+// https://stackoverflow.com/questions/17410809/how-to-calculate-rotation-in-2d-in-javascript
+function rotatePoint (cx, cy, x, y, angle) {
+    const cos = Math.cos(-angle)
+    const sin = Math.sin(-angle)
+    const nx = (cos * (x - cx)) + (sin * (y - cy)) + cx
+    const ny = (cos * (y - cy)) - (sin * (x - cx)) + cy
+    return {x: nx, y: ny}
+}
+
+const getBrushPosition = (painter) => {
+	const x = painter.x - painter.offsetX * painter.scale
+	const y = painter.y - painter.offsetY * painter.scale
+	const cx = painter.x + painter.image.width/2 * painter.scale
+	const cy = painter.y + painter.image.height/2 * painter.scale
+	return rotatePoint(cx, cy, x, y, painter.r)
 }
 
 const updatePainter = (painter, paths, colour) => {
@@ -59,13 +78,15 @@ const updatePainter = (painter, paths, colour) => {
 	const acceleration = painter.acceleration * (Mouse.Right? -1 : 1)
 	painter.speed = clamp(painter.speed + acceleration, painter.minSpeed, painter.maxSpeed)
 
+	const brush = getBrushPosition(painter)
+
 	if (Mouse.Left) {
 		if (!painter.isPainting) {
 			painter.isPainting = true
 			const path = new Path2D()
 			paths.push(path)
 			path.colour = colour
-			path.moveTo(painter.x, painter.y)
+			path.moveTo(brush.x, brush.y)
 		}
 	} else {
 		painter.isPainting = false
@@ -74,7 +95,7 @@ const updatePainter = (painter, paths, colour) => {
 	const [mx, my] = Mouse.position
 	const mouse = {x: mx, y: my}
 
-	const previous = {x: painter.x, y: painter.y, dx: painter.dx, dy: painter.dy}
+	const previous = {x: brush.x, y: brush.y, dx: painter.brushdx, dy: painter.brushdy}
 
 	for (const position of ["x", "y"]) {
 		const speed = `d${position}`
@@ -83,16 +104,21 @@ const updatePainter = (painter, paths, colour) => {
 		painter[position] += painter[speed]
 	}
 
+	painter.r = painter.dx * 0.01 + painter.dy * -0.01
+
+	const newBrush = getBrushPosition(painter)
+
+	painter.brushdx = newBrush.x - brush.x
+	painter.brushdy = newBrush.y - brush.y
+	
 	if (painter.isPainting) {
 		const path = paths.last
 		const angle = Math.atan2(previous.dx, previous.dy)
-		const length = Math.hypot((previous.dx)/2, (previous.dy)/2) / 2
+		const length = Math.hypot((previous.dx / 2), (previous.dy / 2)) / 2
 		const control = {x: length * Math.sin(angle), y: length * Math.cos(angle)}
-		path.quadraticCurveTo(previous.x + control.x, previous.y + control.y, painter.x, painter.y)
+		path.quadraticCurveTo(previous.x + control.x, previous.y + control.y, newBrush.x, newBrush.y)
 		//path.lineTo(painter.x, painter.y)
 	}
-
-	painter.r = painter.dx * 0.01 + painter.dy * -0.01
 
 }
 
