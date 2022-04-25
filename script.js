eab56e55234981e99d65c723e0f68fb6824a98cc
Magnogen <hydronesium@gmail.com>
2022-04-25 19:18:23 +0100

    add variable idle frequencies

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ const makePainter = ({
 	frameRate = 24,
 	speedR = 1.0,
 	idleFadePower = 1.0,
+	idleFrequency = { x: 500, y: 750 }
 } = {}) => {
 
 	const images = []
@@ -146,8 +147,8 @@ const updatePainter = (painter, paths, colour) => {
 		painter[position] += painter[speed]
 	}
 	
-	global.painter.x += (2*Math.sin(performance.now() / 500)) * global.painter.idleFadePower
-	global.painter.y += (2*Math.sin(performance.now() / 600)) * global.painter.idleFadePower
+	global.painter.x += (2*Math.sin(performance.now() / global.painter.idleFrequency.x)) * global.painter.idleFadePower
+	global.painter.y += (2*Math.sin(performance.now() / global.painter.idleFrequency.y)) * global.painter.idleFadePower
 
 	painter.targetR = painter.dx * painter.dr + painter.dy * -painter.dr
 	painter.r += (painter.targetR - painter.r) * painter.speedR
